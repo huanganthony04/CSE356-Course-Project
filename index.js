ad4e752fd6fe6328c9da593794470082f27e6315
Anthony Huang <huanganthony76@gmail.com>
2024-09-29 13:39:59 -0400

    Added CSE-356 Header

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const app = express();
 
 const PORT = 8080;
 
+//Add custom header
+app.use((req, res, next) => {
+    res.header('X-CSE-356', '66d155ec7f77bf55c50044bf');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
+
 
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
