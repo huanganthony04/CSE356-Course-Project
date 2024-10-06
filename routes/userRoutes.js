0c7ba030ead2769f98f9642cf479844662981179
root <anthony.huang.2@stonybrook.edu>
2024-10-06 02:11:02 +0000

    Fully implemented user verification and creation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,13 +4,13 @@ const bcrypt = require('bcrypt');
 const UserModel = require('../models/User');
 const nodemailer = require('nodemailer');
 
-
 const router = express.Router();
 
 router.post('/adduser', async (req, res) => {
     const { username, password, email } = req.body;
     if (!username || !password || !email) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'Missing required fields' });
+        console.log("Missing required fields");
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'Missing required fields' });
     }
     //Check if the username already exists
     let existingUser = await UserModel.findOne({
@@ -20,7 +20,8 @@ router.post('/adduser', async (req, res) => {
         ]
     });
     if (existingUser) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'User already exists' });
+        console.log("User already exists");
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'User already exists' });
     }
 
     //Create a random verification key
@@ -52,17 +53,22 @@ router.post('/adduser', async (req, res) => {
         from: 'root@anthonysgroup.cse356.compas.cs.stonybrook.edu',
         to: email,
         subject: 'Verify your email',
-        text: `Please verify your email using the following link: ${link}`
+        text: `${link}`
     };
     await transporter.sendMail(mailOptions).catch((err) => {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: `${err}` })
+        console.log("Error sending email: " + err);
+        return res.status(200).json({ status: 'ERROR', error: true, message: `${err}` })
     });
+
+    console.log("Email sent");
 
     //Save the user
     await user.save().catch((err) => {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: `${err}` })
+        console.log("Error saving user: " + err);
+        return res.status(200).json({ status: 'ERROR', error: true, message: `${err}` })
     });
 
+    console.log("User created successfully");
     return res.status(200).json({ status: 'OK' });
 });
 
@@ -71,45 +77,49 @@ router.get('/verify', async (req, res) => {
     email = decodeURIComponent(email);
     console.log('Verifying' + email, key);
     if (!email || !key) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'Missing required fields' });
+        console.log("Missing required fields");
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'Missing required fields' });
     }
     let user = await UserModel.findOne({ email });
     if (!user) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'User with email does not exist' });
+        console.log("User with email does not exist");
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'User with email does not exist' });
     }
     if (user.verified) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'User is already verified' });
+        console.log("User is already verified");
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'User is already verified' });
     }
     if (key !== user.verificationKey) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'Invalid verification key' });
+        console.log("Invalid verification key");
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'Invalid verification key' });
     }
     user.verified = true;
 
-    user.save()
-        .then(() => {
-            return res.status(200).json({ status: 'OK', error: 'false', message: 'User verified successfully' });
-        })
-        .catch((err) => {
-            return res.status(200).json({ status: 'ERROR', error: 'true', message: 'Error verifying user' });
-        });
+    await user.save().catch((err) => {
+            console.log("Error verifying user: " + err);
+            return res.status(200).json({ status: 'ERROR', error: true, message: 'Error verifying user' });
+    });
+    
+    console.log("User verified successfully");
+    return res.status(200).json({ status: 'OK' });
 });
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'Missing required fields' });
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'Missing required fields' });
     }
     const user = await UserModel.findOne({ username });
     if (!user) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'User does not exist' });
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'User does not exist' });
     }
     if (!user.verified) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'User is not verified' });
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'User is not verified' });
     }
     
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'Invalid credentials' });
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'Invalid credentials' });
     }
     req.session.isAuth = true;
     res.redirect('/');
@@ -117,11 +127,11 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
     if (!req.session.isAuth) {
-        return res.status(200).json({ status: 'ERROR', error: 'true', message: 'You are not logged in' });
+        return res.status(200).json({ status: 'ERROR', error: true, message: 'You are not logged in' });
     }
     req.session.destroy();
     res.clearCookie('connect.sid');
-    res.redirect('/');
+    res.status(200).json({ status: 'OK' });
 })
 
 module.exports = router;
