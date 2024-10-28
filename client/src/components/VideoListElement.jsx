a9c351b2f6750ef3223099261f4c9553acb18a77
root <anthony.huang.2@stonybrook.edu>
2024-10-28 16:47:13 +0000

    Thumbnails clickable and redirects to player

--- client/src/components/VideoListElement.jsx
+++ client/src/components/VideoListElement.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './VideoListElement.css'
 
 const VideoListElement = (props) => {
 
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/play/${props.id}`);
+  }
 
   return (
-    <div className="video_list_element">
+    <div className="video_list_element" onClick={handleClick}>
       <img className="thumbnail" src={`http://anthonysgroup.cse356.compas.cs.stonybrook.edu/api/thumbnail/${props.id}`} alt="thumbnail"/>
       <h4 className="description">{props.description}</h4>
     </div>
