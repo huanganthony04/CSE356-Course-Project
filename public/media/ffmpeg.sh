b4ca5ec93e4c2561b90fd908599496f8d330f450
Koopertrooper3 <cyu886116@gmail.com>
2024-10-28 23:03:03 -0400

    reencoded 4046200 and 10040768

--- public/media/ffmpeg.sh
+++ public/media/ffmpeg.sh
@@ -29,25 +29,44 @@ declare -a files=(  "2892038-uhd_3840_2160_30fps"
   "4046200-hd_1920_1080_25fps"
   "8721927-uhd_2160_4096_25fps")
 
-for i in "${files[@]}";
-do
+# for i in "${files[@]}";
+# do
 
-    ffmpeg -i ${i}.mp4 \
-    -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 \
-    -b:v:0 254k  -c:v:0 libx264 -filter:v:0 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=320:180 "  \
-    -b:v:1 507k -c:v:1 libx264 -filter:v:1 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=320:180"  \
-    -b:v:2 759k -c:v:2 libx264 -filter:v:2 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=480:270" \
-    -b:v:3 1013k  -c:v:3 libx264 -filter:v:3 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=640:360"  \
-    -b:v:4 1254k -c:v:4 libx264 -filter:v:4 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=640:360"  \
-    -b:v:5 1883k -c:v:5 libx264 -filter:v:5 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=768:432"  \
-    -b:v:6 3134k -c:v:6 libx264 -filter:v:6 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=1024:576"  \
-    -b:v:7 4952k -c:v:7 libx264 -filter:v:7 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=1280:720"  \
-    -use_timeline 1 -use_template 1 -window_size 5 -seg_duration 10 -media_seg_name "${i}_chunk_\$Bandwidth\$_\$Number\$.m4s" -init_seg_name "${i}_init-stream\$RepresentationID\$.\$ext\$" -adaptation_sets "id=0,streams=v" \
-    -hls_playlist false -f dash $i.mpd;
-done
+#     ffmpeg -i ${i}.mp4 \
+#     -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 \
+#     -b:v:0 254k  -c:v:0 libx264 -filter:v:0 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=320:180 "  \
+#     -b:v:1 507k -c:v:1 libx264 -filter:v:1 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=320:180"  \
+#     -b:v:2 759k -c:v:2 libx264 -filter:v:2 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=480:270" \
+#     -b:v:3 1013k  -c:v:3 libx264 -filter:v:3 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=640:360"  \
+#     -b:v:4 1254k -c:v:4 libx264 -filter:v:4 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=640:360"  \
+#     -b:v:5 1883k -c:v:5 libx264 -filter:v:5 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=768:432"  \
+#     -b:v:6 3134k -c:v:6 libx264 -filter:v:6 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=1024:576"  \
+#     -b:v:7 4952k -c:v:7 libx264 -filter:v:7 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=1280:720"  \
+#     -use_timeline 1 -use_template 1 -window_size 5 -seg_duration 10 -media_seg_name "${i}_chunk_\$Bandwidth\$_\$Number\$.m4s" -init_seg_name "${i}_init-stream\$RepresentationID\$.\$ext\$" -adaptation_sets "id=0,streams=v" \
+#     -hls_playlist false -f dash $i.mpd;
+# done
 
 #Neets to be letterbox'd instead
-declare -a files=("5381274-uhd_4096_2160_30fps" "1874710-hd_2048_1024_30fps")
+# declare -a files=("5381274-uhd_4096_2160_30fps" "1874710-hd_2048_1024_30fps")
+# for i in "${files[@]}";
+# do
+
+#     ffmpeg -i ${i}.mp4 \
+#     -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 -map 0:v:0 \
+#     -b:v:0 254k  -c:v:0 libx264 -filter:v:0 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=320:180 "  \
+#     -b:v:1 507k -c:v:1 libx264 -filter:v:1 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=320:180"  \
+#     -b:v:2 759k -c:v:2 libx264 -filter:v:2 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=480:270" \
+#     -b:v:3 1013k  -c:v:3 libx264 -filter:v:3 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=640:360"  \
+#     -b:v:4 1254k -c:v:4 libx264 -filter:v:4 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=640:360"  \
+#     -b:v:5 1883k -c:v:5 libx264 -filter:v:5 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=768:432"  \
+#     -b:v:6 3134k -c:v:6 libx264 -filter:v:6 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=1024:576"  \
+#     -b:v:7 4952k -c:v:7 libx264 -filter:v:7 "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black, scale=1280:720"  \
+#     -use_timeline 1 -use_template 1 -window_size 5 -seg_duration 10 -media_seg_name "${i}_chunk_\$Bandwidth\$_\$Number\$.m4s" -init_seg_name "${i}_init-stream\$RepresentationID\$.\$ext\$" -adaptation_sets "id=0,streams=v" \
+#     -hls_playlist false -f dash $i.mpd;
+# done
+
+#Bespoke encoding
+declare -a files=("10040768-hd_1920_1080_24fps" 4046200-hd_1920_1080_25fps)
 for i in "${files[@]}";
 do
 
@@ -64,8 +83,6 @@ do
     -use_timeline 1 -use_template 1 -window_size 5 -seg_duration 10 -media_seg_name "${i}_chunk_\$Bandwidth\$_\$Number\$.m4s" -init_seg_name "${i}_init-stream\$RepresentationID\$.\$ext\$" -adaptation_sets "id=0,streams=v" \
     -hls_playlist false -f dash $i.mpd;
 done
-
-
 
 # for i in "${files[@]}"
 # do
