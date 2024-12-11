362e35abbf07bd5eae0b34574838887180eca440
Koopertrooper3 <cyu886116@gmail.com>
2024-12-10 21:19:38 -0500

    Added file receiver

---
+++ VideoService/VideoReceiver/receiver.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const busboy = require('busboy')
+const path = require('node:path');
+
+const app = express();
+PORT = 8080
+let ROOT = 'mnt/videoStorage'
+app.post('/',(req, res) => {
+
+    //Body {file: file}
+    
+    //console.log(newuid)
+
+    let reqBody = {}
+    let fieldCount = 0
+    const bb = busboy({ headers: req.headers });
+    bb.on('file', function(filename, file, info) {
+
+        let {ext} = path.parse('/home/user/dir/file.txt');
+
+        let filePath;
+        if(ext == '.jpg'){
+            filePath = path.join(ROOT,'public', 'thumbnails',filename);
+        }else{
+            filePath = path.join(ROOT,'public', 'media',filename);
+        }
+
+        file.pipe(fs.createWriteStream(tempPathFile));
+
+    });
+    bb.on('finish', function() {
+
+        res.status(200).json({status: 'OK'})
+       
+
+    });
+    
+    req.pipe(bb);
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
